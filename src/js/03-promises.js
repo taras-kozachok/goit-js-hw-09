49234273fa2033ec0431349aab8c67a4ca67c73d
taras-kozachok <taras.kozachok.goit@gmail.com>
2022-03-03 13:37:47 +0200

    begin part3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+//const inputFirstDeleyEl = document.querySelector('input[name="delay"]');
+//const inputDeleyStepEl = document.querySelector('input[name="step"]');
+//const inputAmountEl = document.querySelector('input[name="amount"]');
+//const buttonCreatEl = document.querySelector('button[type="submit"]');
+const formEl = document.querySelector('.form');
+
+//console.log(formEl);
+
+formEl.addEventListener("submit", onFormElSubmit);
+ function onFormElSubmit(event) {
+    event.preventDefault();
+   const formElements = event.currentTarget.elements;
+   
+   const delay = formElements.delay.value;
+   const step = formElements.step.value;
+   const amount = formElements.amount.value;
+   console.log(delay);
+  console.log(step);
+   console.log(amount);
+   
+
+  
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
   } else {
-    // Reject
+    // Reject 
   }
 }
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+
